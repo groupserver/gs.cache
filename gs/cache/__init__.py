38e655ea9eff39de83b7dbe2e914256cb450cd89
richard@onlinegroups.net <richard@onlinegroups.net>
2012-07-19 16:46:44 +1200

    Convert to using new config

--- gs/cache/__init__.py
+++ gs/cache/__init__.py
@@ -1,2 +1,2 @@
 # coding=utf-8
-from gs.cache.cache import *
+from gs.cache.cache import cache
--- gs/cache/cache.py
+++ gs/cache/cache.py
@@ -5,48 +5,59 @@
 
 from zope.interface.declarations import implements
 from zope.interface import Interface
-import datetime
-
-import redis
-
 from threading import RLock
-
 from cPickle import dumps, loads
-
+from gs.config import Config, getInstanceId
+from gs.cache.backends import NullCache, RedisCache
+import datetime
 import logging
-log = logging.getLogger('XWFCore.cache')
+log = logging.getLogger('gs.cache')
 
-redisCache = redis.StrictRedis(host='localhost', port=6379, db=0)
+try:
+    import redis
+    haveredis = True
+except ImportError:
+    haveredis = False
 
-class ICache(Interface):
-    def set(key, object): #@NoSelf
-        """ Add an object to the cache.
-        
-        """
-        
-    def has_key(key): #@NoSelf
-        """ Check to see if an object is in the cache.
-        
-        """
-        
-    def get(key): #@NoSelf
-        """ Get an object from the cache by key.
-        
-        """
+class BackendError(Exception):
+    pass
 
-    def remove(key): #@NoSelf
-        """ Remove an object from the cache by key.
+caches = {}
+def get_cache(cache_name, instance_id=None):
+    if not instance_id:
+        instance_id = getInstanceId()
 
-        """
-        
-    def clear(): #@NoSelf
-        """Clear all instances from a cache
-        """
+    # prepend the cache_name with the instance_id to make it unique across
+    # instances
+    cache_name = instance_id+"::"+cache_name
 
+    # yes, we have a cache of caches :-)
+    if caches.has_key(cache_name):
+        return caches[cache_name]
+
+    config = Config(instance_id)
+    config.set_schema('cache', {'backend': str, 'hostname': str, 'port': int})
+    cache_config = config.get('cache')
+    
+    backend = cache_config.get('backend', 'none')
+    
+    if backend == 'redis' and haveredis:
+        redisCache = redis.StrictRedis(host='localhost', port=6379, db=0)
+        caches[cache_name] = RedisCache(redisCache, cache_name)
+    elif backend == 'none':
+        caches[cache_name] = NullCache()    
+    else:
+        raise BackendError(u'No cache backend implemented for "%s"' % backend)
+
+    log.info(u'Intialising cache "%s" using backend "%s"' % (cache_name,backend))
+    
+    return caches[cache_name]
+
+# complex as it looks, this is the main cache decorator
 def cache(cachename, cachekeyfunc, expiry=None):
     def cache_decorator(f):
         def do_cache(*args):
-            cache = SimpleCache(redisCache, cachename)
+            cache = get_cache(cachename)
             cache_key = cachekeyfunc(*args)
             result = cache.get(cache_key)
             if not result:
@@ -58,68 +69,3 @@ def cache(cachename, cachekeyfunc, expiry=None):
         return do_cache # cache_decorator
 
     return cache_decorator # simplecache
-
-class SimpleCache(object):
-    """ Implement ICache with no expiry.
-    
-    """
-    implements(ICache)
-    __thread_lock = RLock()
-    
-    def __init__(self, backend, cache_name):
-        self.backend = backend
-        self.cache_name = cache_name
-        
-    def set(self, key, object, expiry=None):
-        try:
-            if not self.__thread_lock.acquire(False):
-                log.info("Cache (%s), not adding object (%s) to cache, would have required blocking" % (self.cache_name, key))
-                return False
-            fullKey = self.cache_name+'%'+key
-            self.backend.set(fullKey, dumps(object))
-            if expiry:
-                self.backend.expire(fullKey, expiry)
-        finally:
-            try:
-                self.__thread_lock.release()
-            except:
-                pass
-
-        return True
-    
-    def has_key(self, key):
-        fullKey = self.cache_name+'%'+key
-        
-        result = False
-        if self.backend.keys(fullKey):
-            result = True
-        
-        return result        
-
-    def get(self, key):
-        fullKey = self.cache_name+'%'+key
-        result = redisCache.get(fullKey)
-        
-        object = None
-        if result:
-            object = loads(result)        
-
-        return object 
-
-    def delete(self, key):
-        fullKey = self.cache_name+'%'+key
-        redisCache.delete(fullKey)    
-
-    def keys(self):
-        offset = len(self.cache_name)+1
-        outkeys = []
-        search_string = self.cache_name+'%*'
-        for key in self.backend.keys(search_string):
-            outkeys.append(key[offset:])
-        
-        return outkeys
- 
-    def clear(self):
-        search_string = self.cache_name+'%*'
-        for key in self.backend.keys(search_string):
-            self.backend.delete(key)
