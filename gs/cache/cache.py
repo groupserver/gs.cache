8d4f0bb48f5edaaa8a392e1348c67d47ea7665a4
Michael JasonSmith <mpj17@onlinegroups.net>
2012-09-21 08:49:42 +1000

    Minor code cleanup

--- gs/cache/cache.py
+++ gs/cache/cache.py
@@ -38,19 +38,19 @@ def get_cache(cache_name, instance_id=None):
     config = Config(instance_id)
     config.set_schema('cache', {'backend': str, 'hostname': str, 'port': int})
     cache_config = config.get('cache')
-    
+
     backend = cache_config.get('backend', 'none')
-    
+
     if backend == 'redis' and haveredis:
         redisCache = redis.StrictRedis(host='localhost', port=6379, db=0)
         caches[cache_name] = RedisCache(redisCache, cache_name)
     elif backend == 'none':
-        caches[cache_name] = NullCache()    
+        caches[cache_name] = NullCache()
     else:
         raise BackendError(u'No cache backend implemented for "%s"' % backend)
 
     log.info(u'Intialising cache "%s" using backend "%s"' % (cache_name,backend))
-    
+
     return caches[cache_name]
 
 # complex as it looks, this is the main cache decorator
@@ -63,7 +63,7 @@ def cache(cachename, cachekeyfunc, expiry=None):
             if not result:
                 result = f(*args)
                 cache.set(cache_key, result, expiry)
-            
+
             return result # do_cache
 
         return do_cache # cache_decorator
