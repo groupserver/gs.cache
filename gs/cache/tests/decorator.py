b1795bb598e4a8e2d85d1faaf5f3a2a2053e9676
Michael JasonSmith <mpj17@onlinegroups.net>
2014-04-24 16:24:52 -0700

    Adding a couple more tests and some comments.

--- gs/cache/tests/decorator.py
+++ gs/cache/tests/decorator.py
@@ -17,6 +17,8 @@ from unittest import TestCase
 from mock import MagicMock
 import gs.cache.decorator
 
+value = 'value'
+
 
 class FauxCache(object):
     '''A fake cache, for testing the decorator'''
@@ -26,7 +28,7 @@ class FauxCache(object):
     def get(self, *args):
         retval = None
         if self.getHit:
-            retval = 'value'
+            retval = value
         return retval
 
 
@@ -36,10 +38,11 @@ class TestDecorator(TestCase):
         return 'key'
 
     def test_decorator_cache_miss(self):
+        '''Test the decorator when there is a cache-miss'''
         global hasRun
 
         f = FauxCache()
-        f.getHit = False
+        f.getHit = False  # Set the faux-cache up to miss
         f.set = MagicMock()
         gs.cache.decorator.get_cache = MagicMock(return_value=f)
 
@@ -49,14 +52,22 @@ class TestDecorator(TestCase):
         def fn(*args):
             global hasRun
             hasRun = True
-            return 'value'
+            return value
 
         r1 = fn()
+        # The fn shoul be run on a cache-miss.
         self.assertTrue(hasRun)
+        # The value should be set in the cache.
         self.assertEqual(1, f.set.call_count)
-        self.assertEqual('value', r1)
+        args, var_args = f.set.call_args
+        self.assertIn('key', args[0])
+        self.assertEqual(value, args[1])
+        self.assertIs(None, args[2])
+        # The right value should be returned.
+        self.assertEqual(value, r1)
 
     def test_decorator_cache_hit(self):
+        '''Test the decorator on a cache-hit.'''
         global hasRun
 
         f = FauxCache()
@@ -70,9 +81,11 @@ class TestDecorator(TestCase):
         def fn(*args):
             global hasRun
             hasRun = True
-            return 'value'
+            return value
 
         r1 = fn()
+        # If the cache is hit then the fn should not be run.
         self.assertFalse(hasRun)
+        # If the cache is hit then nothing in the cache should be set
         self.assertEqual(0, f.set.call_count)
-        self.assertEqual('value', r1)
+        self.assertEqual(value, r1)
