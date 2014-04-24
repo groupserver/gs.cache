ea775f9131590d8deb362765400f35a9d4a9e11d
Michael JasonSmith <mpj17@onlinegroups.net>
2014-04-24 16:16:04 -0700

    Fixing things in the decorator for hte __future__

--- gs/cache/decorator.py
+++ gs/cache/decorator.py
@@ -12,6 +12,7 @@
 # FOR A PARTICULAR PURPOSE.
 #
 ###############################################################################
+from __future__ import absolute_import, unicode_literals
 from .getcache import get_cache
 
 
