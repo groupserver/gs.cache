175b6cffa3b5ce518cd4c0e42056609f0a0a5945
Michael JasonSmith <mpj17@onlinegroups.net>
2014-04-24 14:45:56 -0700

    Refactoring how the tests are structured.

--- gs/cache/tests/getcache.py
+++ gs/cache/tests/getcache.py
@@ -13,15 +13,14 @@
 #
 ##############################################################################
 from __future__ import absolute_import, unicode_literals
-import time
-from unittest import main, TestSuite, makeSuite, TestCase
+from unittest import TestCase
 from mock import MagicMock
 import gs.cache.backends
 import gs.cache.getcache
 import gs.cache.interfaces
 
 
-class GetCacheTest(TestCase):
+class TestGetCache(TestCase):
 
     def setUp(self):
         gs.cache.getcache.caches = {}  # Because of the cache of caches
@@ -37,58 +36,8 @@ class GetCacheTest(TestCase):
     def test_get_redis_cache(self):
         gs.cache.getcache.haveredis = True
         gs.cache.getcache.get_backend = MagicMock(return_value='redis')
+        gs.cache.getcache.redis = MagicMock()
         r = gs.cache.getcache.get_cache('testcache')
         isCache = gs.cache.interfaces.ICache.providedBy(r)
         self.assertTrue(isCache)
         self.assertIsInstance(r, gs.cache.backends.RedisCache)
-
-    def foo_test_set(self):
-        test_key = 'blargkey'
-        test_value = 'blargvalue'
-
-        haskey = test_key in self.tcache
-        assert not haskey
-
-        self.tcache.set(test_key, test_value)
-        value = self.tcache.get(test_key)
-        assert value == test_value
-
-        haskey = test_key in self.tcache
-        assert haskey
-
-        keys = list(self.tcache.keys())
-        assert len(keys) == 1
-        self.tcache.clear()
-        keys = list(self.tcache.keys())
-        assert len(keys) == 0
-
-    def foo_test_setexpiry(self):
-        test_key = 'blargexpirykey'
-        test_value = 'blargexpiryvalue'
-
-        haskey = test_key in self.tcache
-        assert not haskey
-
-        self.tcache.set(test_key, test_value, 2)
-        value = self.tcache.get(test_key)
-        assert value == test_value
-
-        haskey = test_key in self.tcache
-        assert haskey
-
-        time.sleep(3)
-
-        haskey = test_key in self.tcache
-        assert not haskey
-
-        keys = list(self.tcache.keys())
-        assert len(keys) == 0
-
-
-def test_suite():
-    suite = TestSuite()
-    suite.addTest(makeSuite(GetCacheTest))
-    return suite
-
-if __name__ == '__main__':
-    main(defaultTest='test_suite')
